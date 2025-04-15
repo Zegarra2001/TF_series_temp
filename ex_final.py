e2fbe6d255130ed240ca969f42c01e9d25c239f4
ZelaSR <sergio.zegarra.v@gmail.com>
2025-04-14 19:29:41 -0500

    Update ex_final.py

--- ex_final.py
+++ ex_final.py
@@ -74,5 +74,5 @@ graficar_registro(record, nombre, canal)
 # Obtener rítmo cardiaco
 select_confirmada = False
 
-if all(not select_confirmada, canal != 'Todos'):
+if all([not select_confirmada, canal != 'Todos']):
     select_confirmada = st.button('Calcular FC usando esta derivada', type = 'primary')
