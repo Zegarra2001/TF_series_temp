ce73aaddb456a8d228eeae949d9ae05f8bf8469d
ZelaSR <sergio.zegarra.v@gmail.com>
2025-04-14 19:24:54 -0500

    Update ex_final.py

--- ex_final.py
+++ ex_final.py
@@ -70,3 +70,9 @@ canal = st.selectbox(
 )
 
 graficar_registro(record, nombre, canal)
+
+# Obtener rítmo cardiaco
+select_confirmada = False
+
+while all(not select_confirmada, canal != 'Todos'):
+    select_confirmada = st.button('Calcular FC usando esta derivada', type = 'primary')
