e359c63dd3f85ae739a4ae6c1976e2006082c596
ZelaSR <sergio.zegarra.v@gmail.com>
2025-04-23 11:24:14 -0500

    corrección de scripts

---
+++ utils/plot_utils.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
+def graficar_confusion_matrix(y_true, y_pred, labels):
+    cm = confusion_matrix(y_true, y_pred)
+    plt.figure(figsize=(6,5))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+                xticklabels=labels, yticklabels=labels)
+    plt.xlabel("Predicted")
+    plt.ylabel("True")
+    plt.title("Confusion Matrix")
+    plt.tight_layout()
+    plt.show()
