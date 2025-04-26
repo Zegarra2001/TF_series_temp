dfc12bba67bc110f578602a1e97838818c237843
ZelaSR <sergio.zegarra.v@gmail.com>
2025-04-26 06:09:20 -0500

    Update cnn_model.py

--- models/cnn_model.py
+++ models/cnn_model.py
@@ -8,12 +8,18 @@ class ECGClassifier(nn.Module):
         self.conv1 = nn.Conv1d(1, 16, kernel_size=7, padding=3)
         self.pool = nn.MaxPool1d(2)
         self.conv2 = nn.Conv1d(16, 32, kernel_size=5, padding=2)
-        self.fc1 = nn.Linear(32 * 1250, 64)
-        self.fc2 = nn.Linear(64, 4)  # 4 clases
+
+        dummy_input = torch.zeros(1, 1, 5000)
+        x = self.pool(F.relu(self.conv1(dummy_input)))
+        x = self.pool(F.relu(self.conv2(x)))
+        self.flatten_dim = x.shape[1] * x.shape[2]
+
+        self.fc1 = nn.Linear(self.flatten_dim, 64)
+        self.fc2 = nn.Linear(64, 4)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
-        return x
+        return self.fc2(x)
